56fe4428c14bf6e71666f80ff389bb5567c37ea1
Gloria Villa <hello.gloria@gmail.com>
2024-03-08 01:15:36 -0800

    Initial Commit

---
+++ HarryPotter/HarryPotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterApp.swift
+//  HarryPotter
+//
+//  Created by Gloria Villa on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct HarryPotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
